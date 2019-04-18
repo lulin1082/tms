f1c5d9553e32ed093a49ac287e3e7da73b023d85
lulin108 <lulin108@hotmail.com>
2019-04-18 23:28:25 +0800

    Org 的 后台测试完了，再搞几次真的要用插件了

--- src/main/java/cn/tedu/ttms/system/dao/OrganizationDao.java
+++ src/main/java/cn/tedu/ttms/system/dao/OrganizationDao.java
@@ -20,6 +20,8 @@ public interface OrganizationDao extends BaseDao<Organization> {
     public List<Map<String, Object>> findOrgObjects(@Param("entity")Organization organization,@Param("pageObject")PageObject pageObject) ;
     public int getOrgRowsCounts(@Param("entity")Organization organization);
     public Map<String,Object> findIdAndNames();
-    public int doValidById(@Param(value = "ids") String ids,int valid);
+    public int doValidById(@Param(value = "ids") String[] ids,@Param(value = "valid") Integer valid);
+
+
 }
 
